ded83254cf9ac33cc22ce3ae27fc0db50059e049
tanwei <tan_w@Ctrip.com>
2016-03-19 13:10:03 +0800

    对文件进行整理

---
+++ FastLoginTool/js/background.js
@@ -0,0 +1,71 @@
+﻿var pluginSettings = {  //插件配置
+    enable : true
+};
+
+// 读取localStorage，并把数据推送回content_scripts
+chrome.extension.onRequest.addListener(
+    function (request, sender, sendResponse) {
+        if (request.type == 'getLocalStorage') {
+
+            var fatConfigs = {
+                eid: localStorage.getItem('fat_eid'),
+                ekey: localStorage.getItem('fat_ekey'),
+                modeCode: localStorage.getItem('fat_modeCode'),
+                uid: localStorage.getItem('fat_uid'),
+                subModeCode: localStorage.getItem('fat_subModeCode'),
+                fatEnvir: localStorage.getItem('fat_envir'),
+            };
+            var uatConfigs = {
+                eid: localStorage.getItem('uat_eid'),
+                ekey: localStorage.getItem('uat_ekey'),
+                modeCode: localStorage.getItem('uat_modeCode'),
+                uid: localStorage.getItem('uat_uid'),
+                subModeCode: localStorage.getItem('uat_subModeCode')
+            };
+
+            var prodConfigs = {
+                eid: localStorage.getItem('prod_eid'),
+                ekey: localStorage.getItem('prod_ekey'),
+                modeCode: localStorage.getItem('prod_modeCode'),
+                uid: localStorage.getItem('prod_uid'),
+                subModeCode: localStorage.getItem('prod_subModeCode')
+            };
+
+            var customConfigs = {
+                closeCasoLogin: localStorage.getItem('closeCasoLogin'),
+                closeMessageBox: localStorage.getItem('closeMessageBox'),
+                closeDialogDepartment: localStorage.getItem('closeDialogDepartment')
+            }
+
+            var cookieConfigs = {
+                customCookies: localStorage.getItem('customCookies'),
+                cookiesWriteType: localStorage.getItem('cookiesWriteType'),
+                cookiesWriteUrls: localStorage.getItem('cookiesWriteUrls')
+            }
+
+            sendResponse({
+                pluginEnabled: pluginSettings.enable,
+                fatConfigs: fatConfigs,
+                uatConfigs: uatConfigs,
+                prodConfigs: prodConfigs,
+                customConfigs: customConfigs,
+                cookieConfigs: cookieConfigs
+            });
+
+        }
+    }
+);
+
+// 点击浏览器工具栏上的图标来设置插件是否启用
+chrome.browserAction.onClicked.addListener(function () {
+    if (pluginSettings.enable) {
+        chrome.browserAction.setIcon({ path: 'ctripCorpDisabled.png' });
+        chrome.browserAction.setTitle({ title: '当前为禁用状态..〒▽〒' });
+        pluginSettings.enable = false;
+    }
+    else {
+        chrome.browserAction.setIcon({ path: 'ctripCorp.png' });
+        chrome.browserAction.setTitle({ title: '当前为启用状态~♪(^∇^*)' });
+        pluginSettings.enable = true;
+    }
+});
